2f1e5e2ba4aa8bc00dba7a6c1f38e45522dd36fc
Albert Puigsech Galicia <albert@puigsech.com>
2026-04-13 13:50:44 +0200

    Add Oh My Zsh, Powerlevel10k and custom plugin installation
    Ensures a fresh machine gets OMZ, p10k theme, zsh-autosuggestions
    and zsh-syntax-highlighting installed before dotbot links configs.

---
+++ plugins/zsh/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source ../../lib/logger
+source ../../lib/utils
+
+# Install Oh My Zsh
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+    log_info "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    log_info "Oh My Zsh already installed"
+fi
+
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+# Install Powerlevel10k theme
+if [[ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]]; then
+    log_info "Installing Powerlevel10k..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
+else
+    log_info "Powerlevel10k already installed"
+fi
+
+# Install custom plugins
+declare -A custom_plugins=(
+    [zsh-autosuggestions]="https://github.com/zsh-users/zsh-autosuggestions"
+    [zsh-syntax-highlighting]="https://github.com/zsh-users/zsh-syntax-highlighting"
+)
+
+for plugin in "${!custom_plugins[@]}"; do
+    if [[ ! -d "$ZSH_CUSTOM/plugins/$plugin" ]]; then
+        log_info "Installing $plugin..."
+        git clone --depth=1 "${custom_plugins[$plugin]}" "$ZSH_CUSTOM/plugins/$plugin"
+    else
+        log_info "$plugin already installed"
+    fi
+done
